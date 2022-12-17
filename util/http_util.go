aca3e690eb7a6831d93037b7b526846996b6a82d
yangyang <1348379987@qq.com>
2022-12-17 23:09:46 +0800

    add http_util

---
+++ util/http_util.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func HandleErrorStr(err error, format string, a ...any) bool {
+	if err != nil {
+		log.Println(err)
+		return true
+	}
+	return false
+}
+
+func sendGet(url string) (map[string]interface{}, error) {
+	res, err := http.Get(url)
+	if HandleErrorStr(err, "请求失败;url: %v\n", url) {
+		return nil, err
+	}
+	defer res.Body.Close()
+	contentBytes, err := ioutil.ReadAll(res.Body)
+	HandleErrorStr(err, "response读取异常;url: %v\n", url)
+	var result map[string]interface{}
+	err = json.Unmarshal(contentBytes, &result)
+	if HandleErrorStr(err, "response返回数据json转换异常(%v)", string(contentBytes)) {
+		return nil, err
+	}
+	return result, nil
+}
