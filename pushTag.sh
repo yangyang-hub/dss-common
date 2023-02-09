885e7b30923c64390aa2b30288a12e48cb966049
yangyang <1348379987@qq.com>
2023-02-09 23:34:57 +0800

    v1.0.18

--- pushTag.sh
+++ pushTag.sh
@@ -1,2 +1,2 @@
-git tag -a v1.0.17 -m "v1.0.17"
+git tag -a v1.0.18 -m "v1.0.18"
 git push origin --tags
--- util/stock_util.go
+++ util/stock_util.go
@@ -8,16 +8,14 @@ import (
 
 //单位转换为万
 func UnitConversion(v string) float64 {
-	value := Substr(v, 0, len(v)-1)
+	l := len([]rune(v))
+	value := Substr(v, 0, (l - 2))
 	f, _ := strconv.ParseFloat(value, 64)
 	d := decimal.NewFromFloat(f)
-	unit := Substr(v, len(v)-1, len(v))
+	unit := Substr(v, l-1, l)
 	var result float64 = f
-	switch {
-	case unit == "亿":
-		result, _ = d.Div(decimal.NewFromFloat(10000)).Round(2).Float64()
-	case unit == "万":
-		result = f
+	if unit == "亿" {
+		result, _ = d.Mul(decimal.NewFromFloat(10000)).Round(2).Float64()
 	}
 	return result
 }
