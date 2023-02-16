8f6aa040b11abab9d1285f8e72126273008a7002
yangyang <1348379987@qq.com>
2023-02-16 10:05:16 +0800

    v1.0.27

--- model/live_data_model.go
+++ model/live_data_model.go
@@ -2,12 +2,15 @@ package model
 
 //实时数据
 type LiveData struct {
-	Code          string  `json:"code"`          //编码
-	Name          string  `json:"name"`          //名称
-	Now           float64 `json:"now"`           //当前价
-	Change        float64 `json:"change"`        //涨跌
-	ChangePercent float64 `json:"changePercent"` //涨跌幅
-	Time          string  `json:"time"`          //更新时间
-	Max           float64 `json:"max"`           //最高价
-	Min           float64 `json:"min"`           //最低价
+	Code     string  `json:"code"`      //编码
+	Name     string  `json:"name"`      //名称
+	Now      float64 `json:"now"`       //当前价
+	Change   float64 `json:"change"`    //涨跌
+	PctChg   float64 `json:"pct_chg"`   //涨跌幅
+	Time     string  `json:"time"`      //更新时间
+	Max      float64 `json:"max"`       //最高价
+	Open     float64 `json:"open"`      //开盘价
+	PreClose float64 `json:"pre_close"` //昨收价
+	// Min      float64 `json:"min"`       //最低价
+	// Min      float64 `json:"min"`       //最低价
 }
--- util/stock_util.go
+++ util/stock_util.go
@@ -6,6 +6,34 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+// 计算涨停板
+func CalLimitUp(code string, preClose, change float64, stStock map[string]string) int64 {
+	prefix := Substr(code, 0, 2)
+	var divValue float64 = 10
+	if stStock[code] != "" {
+		divValue = 5
+	} else if prefix == "60" {
+		divValue = 10
+	} else if prefix == "00" {
+		divValue = 10
+	} else if prefix == "68" {
+		divValue = 20
+	} else if prefix == "30" {
+		divValue = 20
+	} else if Substr(code, 0, 1) == "8" {
+		divValue = 30
+	}
+	preClosed := decimal.NewFromFloat(preClose)
+	p := decimal.NewFromFloat(divValue)
+	limit := preClosed.Div(p).Round(2)
+	res := decimal.NewFromFloat(change).Cmp(limit)
+	if res >= 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
 //单位转换为万
 func UnitConversion(v string) float64 {
 	l := len([]rune(v))
